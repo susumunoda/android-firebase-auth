4f88967b500a37c8e005dd4263fb12e07bed2c65
Susumu Noda <susumunoda@gmail.com>
2023-09-15 13:26:41 -0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Android Firebase Auth"
+include(":app")
+include(":library")
